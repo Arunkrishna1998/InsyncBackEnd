a822f0ecc5b3d8b81284352a0c08eef1d10fcb7b
Arunkrishna1998 <arunkvb98@gmail.com>
2024-01-06 17:54:07 +0530

    Updated static root

--- insyncbackend/settings.py
+++ insyncbackend/settings.py
@@ -107,7 +107,7 @@ DATABASES = {
         'USER': 'insyncuser',
         'PASSWORD': 'insync123',
         'HOST': 'localhost',
-        'PORT': ''
+        'PORT': '5432'
     }
 }
 
@@ -147,6 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 MEDIA_URL = 'media/'
 
