91e5cb98cb6438c6c60f90b636b95b2bc60122e1
Arunkrishna1998 <arunkvb98@gmail.com>
2024-01-06 15:26:32 +0530

    Allowed host

--- insyncbackend/settings.py
+++ insyncbackend/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['13.49.44.20','localhost']
 
 SITE_ID=1
 # Application definition
