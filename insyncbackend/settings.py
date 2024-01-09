b009998ec0f4515d33ea2141517d4b054e822868
Arunkrishna1998 <arunkvb98@gmail.com>
2024-01-09 15:21:50 +0530

    Updated Settings.py-4 Changed db

--- insyncbackend/settings.py
+++ insyncbackend/settings.py
@@ -95,14 +95,21 @@ ASGI_APPLICATION = 'insyncbackend.asgi.application'
 
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'dbinsync',
+#         'USER': 'insyncuser',
+#         'PASSWORD': 'insync123',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dbinsync',
-        'USER': 'insyncuser',
-        'PASSWORD': 'insync123',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
